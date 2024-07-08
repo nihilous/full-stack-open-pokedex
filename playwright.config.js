dcf121d8b84b92935a0040f244f6f004140b0497
nihilous <dkfg1@naver.com>
2024-07-08 16:50:34 +0300

    11.9 Simple end-to-end tests, playwrite config fix, test url fix

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './e2e-tests',
   /* Run tests in files in parallel */
-  testIgnore: ['test'],
+  testIgnore: ['**/test/**'],
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
