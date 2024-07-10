8fb69b477fc458139bd4865e5f1abe1855130ad9
nihilous <dkfg1@naver.com>
2024-07-10 16:50:44 +0300

    exercise 11.16 Skipping a commit for tagging and deployment #skip

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('13')
+  res.send('14')
 })
 
 app.get('/health', (req, res) => {
