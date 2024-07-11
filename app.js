b09350dd6ed85de2aeb170476ffa2e558d9bea3f
nihilous <dkfg1@naver.com>
2024-07-11 15:28:32 +0300

    Exercise 11.17 Adding protection to your main branch , ruleset added, testing

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('16')
+  res.send('17')
 })
 
 app.get('/health', (req, res) => {
